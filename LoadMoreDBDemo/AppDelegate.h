4f88b286b384e6c3f2b843505b6e6be9ba0db8bf
杜健林 <dujianlin@dingtalk.com>
2018-07-05 16:53:13 +0800

    Initial Commit

---
+++ LoadMoreDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadMoreDBDemo
+//
+//  Created by do on 2018/7/5.
+//  Copyright © 2018年 do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
